9374bf400d8541b4d0868b42be4c562566a06e3d
Emrikcah <curryandr3@gmail.com>
2020-03-29 14:17:13 -0400

    added logic for addPhraseToDisplay()

--- app.js
+++ app.js
@@ -1,39 +1,48 @@
 const qwerty = document.querySelector('#qwerty');
 const phrase = document.querySelector('#phrase');
 const startBtn = document.querySelector('.btn__reset');
-const phrases = ['dog', 'cat', 'bird', 'fish', 'tiger'];
+const phrases = ['dog gone', 'cat wild', 'bird of prey', 'fish monger', 'tiger and po'];
 let missed = 0; //collect missed guesses
 
 
 let randPhrases = getRandomPhraseAsArray(phrases);
+console.log(randPhrases);
+
 addPhraseToDisplay(randPhrases);
 
 /*****************Functions***************** */
 
 //set the overlay display property to none when the btn is clicked
 function hideOverlay() {
-    //grab the overlay div
     const overlay = document.querySelector('#overlay');
     overlay.style.display = 'none';
-   
+
 }
 
+//get a random index from the array and split it into a new array
 function getRandomPhraseAsArray(arr) {
-    //get a random index from the array and split it into a new array
-    return arr[Math.floor(Math.random() * arr.length)].split(',');
+    return arr[Math.floor(Math.random() * arr.length)].split('');
 }
 
 function addPhraseToDisplay(arr) {
-
+    let ul = document.querySelector('#phrase ul');
     arr.forEach((element) => {
-        let ul = document.querySelector('#phrase ul');
-        let li  = document.createElement('li');
-        //put the phrase inside the li
-        li.innerText = element;
-        //attach the li to its parent ul
-        ul.appendChild(li);
-        
+        if (element === ' ') {
+            let li = document.createElement('li'); //create an li
+            li.classList.add('space'); //add a class to the li
+            li.innerText = element; //put the phrase inside the li
+            ul.appendChild(li); //attach the li to its parent ul
+        } else {
+            let li = document.createElement('li');
+            li.classList.add('letter');
+            li.innerText = element;
+            ul.appendChild(li);
+        }
     });
+}
+
+function checkLetter() {
+
 }
 
 

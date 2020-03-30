a03572fc8b8bdce326291c88a3b16693f5dbf83c
Emrikcah <curryandr3@gmail.com>
2020-03-30 17:43:47 -0400

    added more js logic

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const qwerty = document.querySelector('#qwerty');
 const phrase = document.querySelector('#phrase');
 const startBtn = document.querySelector('.btn__reset');
+
 const phrases = ['dog gone', 'cat wild', 'bird of prey', 'fish monger', 'tiger and po'];
 let missed = 0; //collect missed guesses
 
+
+
 let randPhrases = getRandomPhraseAsArray(phrases);
 addPhraseToDisplay(randPhrases);
-let letterFound = checkLetter();
+
 
 /*****************Functions***************** */
 
@@ -25,41 +28,46 @@ function addPhraseToDisplay(arr) {
     let ul = document.querySelector('#phrase ul');
     arr.forEach((element) => {
         //create an li
-        let li = document.createElement('li'); 
+        let li = document.createElement('li');
         //put the phrase inside the li
-        li.innerText = element; 
+        li.innerText = element;
         //check if there is a space in the phrase
         if (element === ' ') {
             //add a class to the li
-            li.classList.add('space'); 
+            li.classList.add('space');
         } else {
             li.classList.add('letter');
         }
         //attach the li to its parent ul
-        ul.appendChild(li); 
+        ul.appendChild(li);
     });
 }
 
 
-function checkLetter(e) {
-        
-    const btn = e.target
+function checkLetter(btn) {
     const letter = document.querySelectorAll('.letter');
     let match = '';
-    
-        
+
     letter.forEach(element => {
+        
         if (element.textContent === btn.textContent) {
             match = element;
             match.classList.add('show');
             btn.classList.add('chosen');
             btn.disabled = true;
-        } 
-                
+        }
+
     });
     return match;
 }
 
+function newFunction(e) {
+    const btn = e.target
+   
+    let letterFound = checkLetter(btn)
+
+}
+
 /*******************EventListenrs*************** */
 startBtn.addEventListener('click', hideOverlay);
-qwerty.addEventListener('click', checkLetter);
+qwerty.addEventListener('click', newFunction);
